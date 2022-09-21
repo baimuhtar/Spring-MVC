3efa9485a1362696bbbaf14b6d3e74e8944ee684
baimuhtar <baimuhtar1997@gmail.com>
2022-09-21 22:06:46 +0600

    Using view data

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -6,7 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MyController {
     @RequestMapping("/")
-    public String showFirstView(){
+    public String showFirstView() {
         return "first-view";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails() {
+        return "ask-emp-details-view";
+    }
+@RequestMapping("showDetails")
+    public String showEmpDetails() {
+        return "show-emp-details-view";
     }
 }
